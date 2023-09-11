4642a1ae0e7b2d0e47f4100f0d0527529d5ce52b
forgerlil <lilianforger@gmail.com>
2023-09-11 11:54:11 +0200

    DOM manipulation lecture

---
+++ week3/lecture-DOMManipulation/index.js
@@ -0,0 +1,136 @@
+// console.log(document);
+
+// document.children[0].children[1].children[1].style.color = 'red';
+
+// const cardsSection = document.getElementById('articlesSection');
+// const h1 = document.querySelector('h1');
+// const firstCard = document.querySelector('.card');
+
+// querySelector syntax is like CSS Selectors
+// document.querySelector('#articlesSection');
+// document.querySelector('#articlesSection:nth-child(2)');
+// document.querySelector('input[type="email"]');
+
+// console.log(firstCard);
+// console.dir(h1);
+
+// Changing the style or classes of an element:
+// h1.style.color = 'red';
+// h1.style.backgroundColor = 'blue';
+
+// console.log(h1.className);
+// h1.className += ' bg-red-200';
+
+// console.log(h1.classList);
+// h1.classList.add('bg-amber-200', 'text-white');
+// h1.classList.remove('bg-amber-200', 'text-white');
+// h1.classList.toggle('bg-amber-200');
+
+// Changing the content of an element
+// console.log(h1.innerHTML);
+// h1.innerHTML = 'Hello <span class="text-red-600">there!</span>';
+
+// XSS example
+// const form = document.querySelector('form');
+// const searchBar = document.querySelector('#searchBar');
+// const searchResult = document.querySelector('#searchResult');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   searchResult.textContent = searchBar.value;
+// });
+
+// console.log(h1.innerHTML);
+// console.log(h1.innerText);
+// console.log(h1.textContent);
+
+const articles = document.getElementsByTagName('article');
+// const cards = document.getElementsByClassName('card');
+
+// console.log('getting by tag', articles);
+
+// cardsWithQuerySelector.forEach((card) => {
+//   card.classList.toggle('bg-green-300');
+// });
+
+// console.log('getting by querySelector', firstCard);
+
+// firstCard.style.backgroundColor = 'red';
+
+// HTMLCollection vs NodeList (live vs not-live)
+const cardsSection = document.getElementById('articlesSection');
+const cards = document.getElementsByClassName('card');
+const cardsWithQuerySelector = document.querySelectorAll('.card');
+
+// console.log('getting by querySelectorAll', cardsWithQuerySelector);
+// console.log('getting by class', cards);
+
+// const newArticle = document.createElement('article');
+// newArticle.classList.add('card');
+// newArticle.textContent = 'I am here thanks to JavaScript';
+// cardsSection.appendChild(newArticle);
+
+// console.log(cards);
+
+// Creating DOM with methods
+
+const addArticleBtn = document.querySelector('#createArticle');
+
+const appendElements = (parent, ...elements) => {
+  return elements.forEach((element) => parent.appendChild(element));
+};
+
+addArticleBtn.addEventListener('click', () => {
+  const newArticle = document.createElement('article');
+  newArticle.classList.add('card', 'w-96', 'bg-gray-900');
+
+  const cardImg = document.createElement('img');
+  cardImg.src = 'https://picsum.photos/400/300';
+  cardImg.alt = 'A pretty, random image';
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body', 'flex-col');
+
+  const cardH2 = document.createElement('h2');
+  cardH2.classList.add('card-title');
+  cardH2.textContent = 'Featured Article';
+
+  const cardP = document.createElement('p');
+  cardP.textContent = 'I was brought here by JavaScript!';
+
+  const cardBtn = document.createElement('button');
+  cardBtn.classList.add('btn', 'btn-accent', 'w-fit', 'self-end');
+  cardBtn.textContent = 'See more';
+
+  // Adding events
+  // cardBtn.onclick = () => {
+  //   alert('This button was clicked');
+  // };
+
+  // cardBtn.addEventListener('click', () => {
+  //   alert('This button was clicked');
+  // });
+
+  // console.dir(cardBtn);
+
+  appendElements(cardBody, cardH2, cardP, cardBtn);
+  // cardBody.appendChild(cardH2);
+  // cardBody.appendChild(cardP);
+  // cardBody.appendChild(cardBtn);
+
+  appendElements(newArticle, cardImg, cardBody);
+  // newArticle.appendChild(cardImg);
+  // newArticle.appendChild(cardBody);
+
+  cardsSection.appendChild(newArticle);
+});
+
+const firstCard = document.querySelector('.card');
+
+// Removing an element and all its children
+// firstCard.remove();
+
+// This adds a click listener to the window
+// addEventListener('click', () => {
+//   alert('Heyo!');
+// });
