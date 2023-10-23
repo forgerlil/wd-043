5028bea70baec8193d2ea9fc90d103aeed4bfcb2
forgerlil <lilianforger@gmail.com>
2023-10-23 16:12:44 +0200

    travel wishlist correction done

---
+++ week9/correction-travelWishlist/data.js
@@ -0,0 +1,39 @@
+const countries = [
+  {
+    id: 1,
+    name: 'Greece',
+    alpha2Code: 'GR',
+    alpha3Code: 'GRC',
+    visited: false,
+  },
+  {
+    id: 2,
+    name: 'Germany',
+    alpha2Code: 'DE',
+    alpha3Code: 'DEU',
+    visited: true,
+  },
+  {
+    id: 3,
+    name: 'Brazil',
+    alpha2Code: 'BR',
+    alpha3Code: 'BRA',
+    visited: true,
+  },
+  {
+    id: 4,
+    name: 'United Kingdom',
+    alpha2Code: 'GB',
+    alpha3Code: 'GBR',
+    visited: true,
+  },
+  {
+    id: 5,
+    name: 'Czech Republic',
+    alpha2Code: 'CZ',
+    alpha3Code: 'CZE',
+    visited: true,
+  },
+];
+
+module.exports = countries;
