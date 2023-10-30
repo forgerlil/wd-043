b6e95293bc1a5f8316bc4eff8f5f4535f491fb73
forgerlil <lilianforger@gmail.com>
2023-10-30 17:44:59 +0100

    mongo lecture boilerplate

---
+++ week10/lecture-mongoDB/controllers/ownerControllers.js
@@ -0,0 +1,27 @@
+import ErrorStatus from '../utils/errorStatus.js';
+
+const allOwners = async (req, res, next) => {
+  try {
+    return res.json({});
+  } catch (error) {
+    next(error);
+  }
+};
+
+const oneOwner = async (req, res, next) => {
+  try {
+    return res.json({});
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createOwner = async (req, res, next) => {
+  try {
+    return res.json({});
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { allOwners, oneOwner, createOwner };
